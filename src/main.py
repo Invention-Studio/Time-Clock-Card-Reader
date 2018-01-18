7d36aba39e6a54c7c4af686c195595faa7d014f3
Nick's Creative Studio <nickscreativestudio@lawn-143-215-114-105.lawn.gatech.edu>
2018-01-17 20:51:39 -0500

    Updating UI

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+        self.showFullScreen()
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
