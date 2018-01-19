3e3f4eece0748d46ad0454453c78aff613e851aa
Nick's Creative Studio <nickscreativestudio@lawn-143-215-114-105.lawn.gatech.edu>
2018-01-19 04:38:17 -0500

    Adding code to write user file

--- src/ApiFactory.py
+++ src/ApiFactory.py
@@ -1,13 +1,23 @@
 import os
 import shift_planning
-import json
 import credentials as creds
+from UserFactory import UserFactory
 
 def getUsers():
     s = shift_planning.ShiftPlanning(creds.HUMANITY_KEY, creds.HUMANITY_LOGIN, creds.HUMANITY_PASSWORD)
     s.do_login()
     s.get_employees()
-    print s.get_public_data()
+    uf = UserFactory('users.csv')
+    for e in s.get_public_data():
+        firstname = e["firstname"]
+        lastname = e["lastname"]
+        if firstname is None:
+            firstname = ""
+        if lastname is None:
+            lastname = ""
+        else:
+            lastname = lastname + ", "
+        uf.write(e['id'], e['firstname'], e['lastname'], e['username'], 0)
 
 if __name__ == "__main__":
     getUsers()
