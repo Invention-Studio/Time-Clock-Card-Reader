76ed39b30e33a9cc8a6951a81ebe580f25dccffc
Nick's Creative Studio <nickscreativestudio@lawn-143-215-114-105.lawn.gatech.edu>
2018-01-19 04:54:32 -0500

    Adding logic to populate combobox

--- src/AddUserWindow.py
+++ src/AddUserWindow.py
@@ -1,4 +1,5 @@
 from PyQt4 import uic
+from UserFactory import UserFactory
 
 qtcMainWindowFile = "adduserwindow.ui"
 Ui_AddUserWindow, AddUserWindowClass = uic.loadUiType(qtcMainWindowFile)
@@ -8,6 +9,11 @@ class AddUserWindow(AddUserWindowClass, Ui_AddUserWindow):
         AddUserWindowClass.__init__(self)
         Ui_AddUserWindow.__init__(self)
         self.setupUi(self)
+
+        uf = UserFactory('users.csv')
+        users = uf.readAll()
+        print len(users)
+
         self.userDropdown.addItem('Kabbabe, Kristian (kakaday22)', 123)
         self.userDropdown.addItem('Rupert, Nick (nickrupert7)', 789)
 
