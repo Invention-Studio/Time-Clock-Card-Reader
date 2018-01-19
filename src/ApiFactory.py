659e126213eb9465d15c2182beac0a7b281592c5
Nick's Creative Studio <nickscreativestudio@lawn-143-215-114-105.lawn.gatech.edu>
2018-01-19 04:11:29 -0500

    Fixed ApiFactory issue

--- src/ApiFactory.py
+++ src/ApiFactory.py
@@ -6,7 +6,7 @@ import credentials as creds
 def getUsers():
     s = shift_planning.ShiftPlanning(creds.HUMANITY_KEY, creds.HUMANITY_LOGIN, creds.HUMANITY_PASSWORD)
     s.do_login()
-    s.get_employees
+    s.get_employees()
     print s.get_public_data()
 
 if __name__ == "__main__":
