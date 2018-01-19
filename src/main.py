f8e8547348821e21e69bc66051b8f1052c34e3ab
Nick's Creative Studio <nickscreativestudio@lawn-143-215-114-105.lawn.gatech.edu>
2018-01-19 02:08:48 -0500

    Cleaning up card reader threads

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ class MyApp(QtGui.QMainWindow):
         #self.showFullScreen()
 
     def addUser(self):
-        self.addUserWindow.cardReaderThread.stop()
+        self.mainWindow.cardReaderThread.stop()
         if self.addUserWindow is None:
             self.addUserWindow = AddUserWindow(self)
         self.addUserWindow.backButton.clicked.connect(self.exitAddUser)
@@ -27,7 +27,7 @@ class MyApp(QtGui.QMainWindow):
     def exitAddUser(self):
         self.central_widget.setCurrentWidget(self.mainWindow)
         self.central_widget.removeWidget(self.addUserWindow)      
-        self.addUserWindow.cardReaderThread.start()
+        self.mainWindow.cardReaderThread.start()
         
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
