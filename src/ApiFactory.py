471dd7edc82a5712d1542e392df96bb35cb9e92b
Nick's Creative Studio <nickscreativestudio@lawn-143-215-114-105.lawn.gatech.edu>
2018-01-19 04:00:44 -0500

    Adding API logic

---
+++ src/ApiFactory.py
@@ -0,0 +1,13 @@
+import os
+import shift_planning
+import json
+import credentials as creds
+
+def getUsers():
+    s = shift_planning.ShiftPlanning(creds.HUMANITY_KEY, creds.HUMANITY_LOGIN, creds.HUMANITY_PASSWORD)
+    s.do_login()
+    s.get_employees
+    print s.get_public_data()
+
+if __name__ == "__main__"
+    getUsers()
