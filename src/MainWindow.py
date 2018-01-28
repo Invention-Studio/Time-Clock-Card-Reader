dd0b01aa859057019d4e10e160e09d0900871be6
Nick Rupert <nickrupert7@gmail.com>
2018-01-28 17:36:22 -0500

    Testing

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -11,19 +11,19 @@ class MainWindow(MainWindowClass, Ui_MainWindow):
         MainWindowClass.__init__(self)
         Ui_MainWindow.__init__(self)
         self.parent = parent
-        self.something = "testing"
         self.setupUi(self)
 
         self.uf = UserFactory('users.csv')
 
     @QtCore.pyqtSlot(QString)
     def cardScanned(self, card):
-        print self.something
-        userid = self.uf.read(card)[1]
-        user = InternetClient.getUserDetails(userid)
-        status = InternetClient.getUserStatus(userid, 0)
-        statusDetails = None
-        if status == "in":
-            statusDetails = InternetClient.getUserStatus(userid, 1)
+         print QtCore.QThread.currentThread()
+#        userid = self.uf.read(card)[1]
+#        user = InternetClient.getUserDetails(userid)
+#        status = InternetClient.getUserStatus(userid, 0)
+#        statusDetails = None
+#        if status == "in":
+#            statusDetails = InternetClient.getUserStatus(userid, 1)
 
-        self.parent.startLogin(user["realname"], status, user["last_visit"])
+#        self.parent.startLogin(user["realname"], status, user["last_visit"])
+        self.parent.startLogin(None, None, None)
