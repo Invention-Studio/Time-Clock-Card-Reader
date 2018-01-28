7f2bbfdf203a1491f174860846719bc998f89cc3
Nick Rupert <nickrupert7@gmail.com>
2018-01-28 15:22:52 -0500

    Fixed user info

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -16,7 +16,7 @@ class MainWindow(MainWindowClass, Ui_MainWindow):
 
     def cardScanned(self, card):
         userid = self.uf.read(card)[1]
-        employeeInfo = InternetClient.getUserDetails(userid)
+        user = InternetClient.getUserDetails(userid)
         status = InternetClient.getUserStatus(userid, 0)
         statusDetails = None
         if status == "in":
