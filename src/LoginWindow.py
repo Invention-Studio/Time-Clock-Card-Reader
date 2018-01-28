769aaafc5078ffdc5e6314875121868f60fa077b
Nick Rupert <nickrupert7@gmail.com>
2018-01-28 15:47:23 -0500

    Fixed typo

--- src/LoginWindow.py
+++ src/LoginWindow.py
@@ -4,7 +4,7 @@ qtcLoginWindowFile = "loginwindow.ui"
 Ui_LoginWindow, LoginWindowClass = uic.loadUiType(qtcLoginWindowFile)
 
 class LoginWindow(LoginWindowClass, Ui_LoginWindow):
-    def __init__(self, parent=None, realName=None, status=None, lastClockIn=None:
+    def __init__(self, parent=None, realName=None, status=None, lastClockIn=None):
         LoginWindowClass.__init__(self)
         Ui_LoginWindow.__init__(self)
         self.parent = parent
